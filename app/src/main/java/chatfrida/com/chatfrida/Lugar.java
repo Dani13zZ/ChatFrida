adc0a917e652e821fad1fe651faea356bda4df4c
DanielPB <danip.xb@gmail.com>
2018-11-20 23:49:01 -0600

    commit

---
+++ app/src/main/java/chatfrida/com/chatfrida/Lugar.java
@@ -0,0 +1,26 @@
+package chatfrida.com.chatfrida;
+
+public class Lugar {
+
+    public String Direccion;
+    public String Comida;
+    public String Nombre;
+    public int HorarioFin;
+    public int HorarioInicio;
+    public double Latitud;
+    public double Longitud;
+    public double Puntaje;
+
+    public Lugar(String direccion,String comida, String nombre, int horarioFin, int horarioInicio, double latitud, double longitud, double puntaje) {
+        this.Direccion = direccion;
+        this.Nombre = nombre;
+        this.Comida = comida;
+        this.HorarioFin = horarioFin;
+        this.HorarioInicio = horarioInicio;
+        this.Latitud = latitud;
+        this.Longitud = longitud;
+        this.Puntaje = puntaje;
+    }
+    public  Lugar(){}
+
+}
